bc7f41483766481fd4ac637b41d9ae550e0a5557
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2021-07-10 01:50:32 +0300

    Add modal

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,24 @@ headerCityButton.addEventListener('click', () => {
   headerCityButton.textContent = city;
   localStorage.setItem('lomoda-location', city)
 });
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+  cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+  cartOverlay.classList.remove('cart-overlay-open');
+};
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+    cartModalClose();
+  }
+});
