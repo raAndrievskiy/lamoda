b22a09867391aa70686d73ad6f0fa2bd08db363a
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2021-07-11 01:56:20 +0300

    Block scroll modal

--- scripts/script.js
+++ scripts/script.js
@@ -9,15 +9,43 @@ headerCityButton.addEventListener('click', () => {
   localStorage.setItem('lomoda-location', city)
 });
 
+// Блокировка скрола
+
+const desableScroll = () => {
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+  document.body.dbScrollY = window.scrollY;
+
+  document.body.style.cssText = `
+    position: fixed;
+    top: ${-window.scrollY}px;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    padding-right: ${widthScroll}px;
+  `;
+};
+const enableScroll = () => {
+  document.body.style.cssText = '';
+  window.scroll({
+    top: document.body.dbScrollY
+  })
+};
+
+// Модальное окно
+
 const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
 
 const cartModalOpen = () => {
   cartOverlay.classList.add('cart-overlay-open');
+  desableScroll();
 };
 
 const cartModalClose = () => {
   cartOverlay.classList.remove('cart-overlay-open');
+  enableScroll();
 };
 
 subheaderCart.addEventListener('click', cartModalOpen);
